12bac2d12b6c96c65321aff3a4d86d8c03b36d25
Fibonachi-o <fobonacioleg@gmail.com>
2026-07-27 23:21:28 +0300

    Add C# basics projects for days 1-3

---
+++ 01_Basics/Day03_Conditions/Day03_Conditions/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите возраст:");
+int age = int.Parse(Console.ReadLine());
+if (age < 18)
+{
+    Console.WriteLine("Доступ запрещен");
+    Console.WriteLine("Вы несовершеннолетний");
+}
+
+else if (age <= 59)
+{
+    Console.WriteLine("Добро пожаловать!!");
+    Console.WriteLine("Вы совершеннолетний");
+}
+
+else
+{
+    Console.WriteLine("Вы пенсионер");
+}
