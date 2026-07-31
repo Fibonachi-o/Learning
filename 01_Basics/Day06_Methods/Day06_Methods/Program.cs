1ffb3a808fa9be365beec1ca255db4c853f3faae
Fibonachi-o <fobonacioleg@gmail.com>
2026-07-31 22:41:40 +0300

    Day 6: Learning methods and return

---
+++ 01_Basics/Day06_Methods/Day06_Methods/Program.cs
@@ -0,0 +1,59 @@
+﻿int result = Square(5);
+Console.WriteLine(result);
+
+static int Square(int number)
+{
+    return number * number;
+}
+
+/*
+static int Sum(int a, int b)
+{
+    return a + b;
+}
+
+Console.WriteLine(Sum(3, 4));
+*/
+
+/*
+Greet("Олег");
+Greet("Анна");
+Greet("Мия");
+static void Greet(string name)
+{
+    Console.WriteLine($"Привет, {name}!");
+    Console.WriteLine("Привет, " + name);
+}
+*/
+
+
+
+/*
+PrintMessage("Доброе утро!");
+PrintMessage("Сегодня изучаем параметры.");
+PrintMessage("Hello!");
+
+static void PrintMessage(string message)
+{
+    Console.WriteLine(message);
+}
+*/
+
+
+/*
+PrintTitle();
+
+static void PrintLine()
+{
+    Console.WriteLine("--------------------");
+}
+
+static void PrintTitle()
+{
+    PrintLine();
+    Console.WriteLine("Изучаем методы");
+    PrintLine();
+}
+*/
+
+
