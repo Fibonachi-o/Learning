c4c001d9583585c50d1fb6826c41a067022f8dbf
Fibonachi-o <fobonacioleg@gmail.com>
2026-08-01 19:49:13 +0300

    Repeat methods

--- 01_Basics/Day06_Methods/Day06_Methods/Program.cs
+++ 01_Basics/Day06_Methods/Day06_Methods/Program.cs
@@ -1,10 +1,49 @@
-﻿int result = Square(5);
+﻿Sum(5, 2);
+Multiply(5, 2);
+Max(5, 2);
+IsEven(8);
+
+static int Sum(int a, int b)
+{
+    return a + b;
+}
+
+static int Multiply(int a, int b)
+{
+    return a * b;
+}
+
+static int Max(int a, int b)
+{
+    if (a > b)
+        return a;
+    else
+        return b;
+}
+
+static bool IsEven(int number)
+{
+    return number % 2 == 0;
+}
+
+
+
+
+
+
+
+
+
+
+/*
+int result = Square(5);
 Console.WriteLine(result);
 
 static int Square(int number)
 {
     return number * number;
 }
+*/
 
 /*
 static int Sum(int a, int b)
