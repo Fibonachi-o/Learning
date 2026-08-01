7da64ca96f61192b603329bc5773d0f79136927c
Fibonachi-o <fobonacioleg@gmail.com>
2026-08-01 21:17:37 +0300

    Day07: Learning nested loops

---
+++ 01_Basics/Day07_NestedLoops/Day07_NestedLoops/Program.cs
@@ -0,0 +1,17 @@
+﻿int height = 5;
+
+for (int i = 1; i <= height; i++)
+{
+    int stars = 2 * (height - i) + 1;
+    for (int j = 0; j < i - 1; j++)
+    {
+        Console.Write(" ");
+    }
+
+    for (int k = stars; k >= 1; k--)
+    {
+        Console.Write("*");
+    }
+    Console.WriteLine();
+}
+
