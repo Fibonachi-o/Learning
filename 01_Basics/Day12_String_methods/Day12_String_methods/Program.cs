e3eae5c87b52df1b4cd68c26eec4729057cef349
Fibonachi-o <fobonacioleg@gmail.com>
2026-08-11 20:43:44 +0300

    Day12: Learning methods for string

---
+++ 01_Basics/Day12_String_methods/Day12_String_methods/Program.cs
@@ -0,0 +1,30 @@
+﻿/* //Проверка на содержание в конце строки
+Console.WriteLine("Введите почту:");
+string email = Console.ReadLine();
+if (email.EndsWith("@gmail.com") ||
+    email.EndsWith("@yandex.ru"))
+{
+    Console.WriteLine("Почта поддерживается.");
+}
+else
+{
+    Console.WriteLine("Неизвестный почтовой сервис.");
+}
+*/
+
+Console.WriteLine("Введите предложение:");
+string text = Console.ReadLine();
+
+string[] words = text.Split(' ', ',', StringSplitOptions.RemoveEmptyEntries);
+int count = 0;
+
+for (int i = 0; i < words.Length; i++)
+{
+    Console.WriteLine(words[i]);
+    // if (words[i] != "")  //Подсчёт слов без параметра StringSplitOptions.RemoveEmptyEntries
+    // {
+    //     count++;
+    // }
+}
+//Console.WriteLine("Количество слов: " + count); //Подсчёт слов без параметра StringSplitOptions.RemoveEmptyEntries
+Console.WriteLine("Количество слов: " + words.Length);
